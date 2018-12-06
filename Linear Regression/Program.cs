c02c92bfc71e14b1a4808583a2243b0188df7059
Justin Brüffer <jbrueffer@hrnetzwerk.de>
2018-12-06 16:58:05 +0100

    Linear Progressing

---
+++ Linear Regression/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Linear_Regression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inputs = { 80, 60, 10, 20, 30 };
+            double[] outputs = { 20, 40, 30, 50, 60 };
+        }
+    }
+}
